fa477782697b3667be4d16592d3985525751468f
unknown <rasyabayu857@gmail.com>
2024-12-05 14:02:27 +0700

    fix url

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,20 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from "./components/Home"; 
-import Login from "./components/Login.jsx"; 
-import Register from "./components/Register"; 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login.jsx";
+import Register from "./components/Register";
 
-
-  
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Login" element={<Login />} /> 
-        <Route path="/Register" element={<Register />} /> 
-   
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
-   );
-  
-  };
-
-// const App = () => {
-  
-//   return (
-//     <>
-//       <Home />
-//       <Login />
-    
-//     </>
-//   );
-// };
-
+  );
+};
 
 export default App;
